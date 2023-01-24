3c7f844457a50cc86eae86f0af9c5aad8dcc0b5a
Alekhya Vuppu <84461939+alekhya559@users.noreply.github.com>
2023-01-24 15:39:02 +0530

    Update Demo1234.java

--- Demo1234.java
+++ Demo1234.java
@@ -1,5 +1,8 @@
 class Demo1234
 {public static void main(String args[])
 {
-System.out.print("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
-}}
+ int n = 4;
+         for(int i=1;i<=10;i++) {
+        	 System.out.println(4 +" * " + i + " = " + 4*i ); 
+         }
+	}}
